2ece7fb9dc28f98bd1e8548bc7977f945a7cb234
ax4987001 <ax4987@gmail.com>
2023-08-11 10:58:01 +0800

    code

--- src/main/java/com/ax4987/magicandordinary/potion/PotionChineseMoslaHerb.java
+++ src/main/java/com/ax4987/magicandordinary/potion/PotionChineseMoslaHerb.java
@@ -10,9 +10,9 @@ import net.minecraft.util.ResourceLocation;
 public class PotionChineseMoslaHerb extends Potion {
     public static final ResourceLocation ICON = new ResourceLocation(MAGICandordinary.MODID +":textures/gui/potion_icons.png");
     public PotionChineseMoslaHerb() {
-        super(false, 0x800080); //第一个参数表示药水效果是否为负面效果，这里我们设置为false；第二个参数表示药水的颜色
-        setRegistryName(MAGICandordinary.MODID,"mosla_herb"); //设置药水效果的注册名字
-        setPotionName("potion.mosla_herb"); //设置药水效果的本地化名字
+        super(false, 0x800080);
+        setRegistryName(MAGICandordinary.MODID,"mosla_herb");
+        setPotionName("potion.mosla_herb");
     }
     @Override
     public void performEffect(EntityLivingBase entityLivingBaseIn, int amplifier){
